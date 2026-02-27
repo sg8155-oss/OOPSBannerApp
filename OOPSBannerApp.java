2cb3e0bf904c923748fb6d680c8ffdbc97d59a0e
sg8155-oss <sg8155@srmist.edu.in>
2026-02-27 21:22:22 +0530

    UC4: Render OOPS banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,18 @@
+public class OOPSBannerApp{
 
-public class OOPSBannerApp {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
 
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", " ***** ", " ***** "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*     *", "*     "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "* **** ", " ***** "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*     ", "       *"));
-
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", "*     ", "  ***** "));
-
-    }
-
+   String[] lines = new String[9];
+   lines[0] = String.join(" ", "   ***   ", "   ***   ", "******   ", "   *****");
+   lines[1] = String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **     ");     
+   lines[2] = String.join(" ", "**     **", "**     **", "**     **", "**      ");
+   lines[3] = String.join(" ", "**     **", "**     **", "**    ** ", " **     ");
+   lines[4] = String.join(" ", "**     **", "**     **", "******   ", "   ***  ");
+   lines[5] = String.join(" ", "**     **", "**     **", "**       ", "     ** ");
+   lines[6] = String.join(" ", "**     **", "**     **", "**       ", "      **");
+   lines[7] = String.join(" ", " **   ** ", " **   ** ", "**       ", "     ** ");
+   lines[8] = String.join(" ", "   ***   ", "   ***   ", "**       ", "*****   ");
+  for(String line : lines){ 
+    System.out.println(line):
+   }
 }
-
-
